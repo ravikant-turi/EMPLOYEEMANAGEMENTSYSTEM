c32d64fee859ca86bd592114a67298c59c56ee76
Ravikant Turi <145963203+ravikant-turi@users.noreply.github.com>
2025-09-13 16:42:02 +0530

    rouint is implemented

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -4,12 +4,23 @@ import HeaderComponent from "./component/HeaderComponent";
 import ListOfEmployeeComponent from "./component/ListOfEmployeeComponent";
 import HelloWorld from "./HelloWorld";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <HeaderComponent />
-      <ListOfEmployeeComponent />
-      <FooterComponent/>
+      <BrowserRouter>
+        <HeaderComponent />
+
+        <Routes>
+{/* http://localhost:3002/ */}
+          <Route path="/" element={<ListOfEmployeeComponent />} />
+{/* http://localhost:3002/employees */}
+          <Route path="/employees" element={<ListOfEmployeeComponent />} />
+        </Routes>
+
+        <FooterComponent />
+      </BrowserRouter>
     </>
   );
 }
